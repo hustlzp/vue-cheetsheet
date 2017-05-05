33f0c0a86d1617c046d7853e9d431410d1d53977
Zhipeng Liu <hustlzp@qq.com>
2017-05-05 11:07:34 +0800

    Create fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from fabric.api import run, env, cd
+
+
+def deploy():
+    """部署"""
+    env.host_string = "user@server"
+    with cd('/path/to/project'):
+        run('git pull')
+        run('npm run install')
+        run('npm run build')
